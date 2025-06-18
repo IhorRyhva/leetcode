e68461cdbcc75d5e0e80853a4a7cdfb968fb6b5d
Admin <rykhvaihor@gmail.com>
2025-06-18 22:48:40 +0300

    Start

---
+++ src/LeetCode/PalindromeNumber.java
@@ -0,0 +1,14 @@
+package LeetCode;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        PalindromeNumber palindromeNumber = new PalindromeNumber();
+        System.out.println(palindromeNumber.isPalindrome(-22));
+    }
+
+    public boolean isPalindrome(int x) {
+        String number = x + "";
+        StringBuilder reversedNumber = new StringBuilder(number);
+        return number.equals(reversedNumber.reverse().toString());
+    }
+}
