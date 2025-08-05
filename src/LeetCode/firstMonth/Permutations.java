2d3cce6a58dc74a8853922dbfaacffbb6cd6ae40
Admin <rykhvaihor@gmail.com>
2025-08-05 16:51:13 +0300

    Start

--- src/LeetCode/firstMonth/Permutations.java
+++ src/LeetCode/firstMonth/Permutations.java
@@ -4,14 +4,15 @@ package LeetCode.firstMonth;
 public class Permutations {
     public static void main(String[] args) {
         Permutations permutations = new Permutations();
-        System.out.println(permutations.fibonachi(8));
+        System.out.println(permutations.fibonachi(7));
+        System.out.println(permutations.fibonachiRec(7));
     }
 
     private int fibonachi(int flag){
-        int first = 0;
+        int first = 1;
         int second = 1;
         int result = 0;
-        if(flag < 3){
+        if(flag < 2){
             return 1;
         }
         for (int i = 2; i < flag; i++) {
@@ -21,6 +22,13 @@ public class Permutations {
             result = second + first;
         }
         return result;
+    }
+    private int fibonachiRec(int flag){
+        if(flag < 2){
+            return 1;
+        }else{
+            return fibonachiRec(flag - 1) + fibonachiRec(flag - 2);
+        }
     }
 
 }
